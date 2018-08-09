eabe8aa8de02baa3370069274e3c56317ca178f8
Adi Sujiwo <sujiwo@gmail.com>
2018-08-09 15:20:01 +0900

    Compiling dataset_browser

--- ros/src/computing/perception/localization/packages/oculus_localizer/src/bin/dataset_browser.cpp
+++ ros/src/computing/perception/localization/packages/oculus_localizer/src/bin/dataset_browser.cpp
@@ -5,7 +5,40 @@
  *      Author: sujiwo
  */
 
+#include <iostream>
+
+#include <QApplication>
+#include <QWidget>
+
+#include "ui_dataset_browser.h"
 #include "GenericDataset.h"
 #include "OxfordDataset.h"
 
 
+using namespace std;
+
+
+class DatasetBrowserFrm : public QWidget
+{
+//Q_OBJECT
+
+private slots:
+	void on_timelineSlider_sliderMoved(int value)
+	{
+		cout << value << endl;
+	}
+};
+
+
+int main(int argc, char *argv[])
+{
+	QApplication datasetBrowserApp(argc, argv);
+
+	DatasetBrowserFrm window;
+	Ui::DatasetBrowser_frm dbfrm;
+	dbfrm.setupUi(&window);
+
+	window.show();
+
+	return datasetBrowserApp.exec();
+}
