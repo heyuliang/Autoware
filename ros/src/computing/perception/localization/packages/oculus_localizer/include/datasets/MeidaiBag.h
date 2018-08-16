ae30fb6947c64ba98d570fa066ec186dfce879b8
Adi Sujiwo <sujiwo@gmail.com>
2018-08-16 16:25:26 +0900

    Prototype

--- ros/src/computing/perception/localization/packages/oculus_localizer/include/datasets/MeidaiBag.h
+++ ros/src/computing/perception/localization/packages/oculus_localizer/include/datasets/MeidaiBag.h
@@ -8,13 +8,37 @@
 
 #include <string>
 #include <memory>
+#include <vector>
 #include <rosbag/bag.h>
+
+#include "utilities.h"
 #include "datasets/GenericDataset.h"
 #include "datasets/RandomAccessBag.h"
 
 
 #ifndef _MEIDAIBAG_H_
 #define _MEIDAIBAG_H_
+
+
+class PoseStamped : public Pose
+{
+protected:
+	ros::Time timestamp;
+};
+
+
+class Trajectory : public vector<PoseStamped>
+{
+public:
+
+	void push_back(const PoseStamped &);
+
+	PoseStamped at(const ros::Time&) const;
+	PoseStamped at(const double) const;
+
+	PoseStamped interpolate (const double) const;
+	PoseStamped interpolate (const ros::Time&) const;
+};
 
 
 class MeidaiDataItem : public GenericDataItem
