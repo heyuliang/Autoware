2a4b89bb9ee7cf5feb79b9bd4d39bf7428f11a8c
Yuki Iida <yuki.iida@tier4.jp>
2018-08-21 16:05:11 +0900

    support AWS IoT

--- ros/src/socket/packages/mqtt_socket/nodes/mqtt_socket.py
+++ ros/src/socket/packages/mqtt_socket/nodes/mqtt_socket.py
@@ -4,7 +4,9 @@ import os, sys
 import rospy
 import tf
 import yaml
+import traceback
 import paho.mqtt.client as mqtt
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 from geometry_msgs.msg import PoseStamped, TwistStamped
 from autoware_msgs.msg import RemoteCmd, VehicleStatus, TrafficLightResult, state
 
@@ -14,10 +16,17 @@ pub_remote_cmd = None
 
 def publish_mqtt_msg(topic, msg):
     if mqtt_cient is not None:
-        mqtt_cient.publish(
-            "vehicle/" + str(mqtt_config["mqtt"]["VEHICLEID"]) + "/" + topic,
-            msg
-        )
+        if mqtt_config['mqtt']['TYPE'] == "MQTT":
+            mqtt_cient.publish(
+                "vehicle/" + str(mqtt_config["mqtt"]["VEHICLEID"]) + "/" + topic,
+                msg
+            )
+        elif mqtt_config['mqtt']['TYPE'] == "AWS_IOT":
+            mqtt_cient.publish(
+                "vehicle/" + str(mqtt_config["mqtt"]["VEHICLEID"]) + "/" + topic,
+                msg,
+                0
+            )
 
 def convert_csv(data_array):
     csv_msg = ""
@@ -28,13 +37,13 @@ def convert_csv(data_array):
 def vehicle_status_callback(data):
     if data.header.seq % (float(mqtt_config["mqtt"]["VEHICLESTATUS_DOWNSAMPLE"]) * 100) == 0:
         gearshift = -1
-        if int(gearshift) == int(mqtt_config["mqtt"]["GEAR_D"]):
+        if int(data.gearshift) == int(mqtt_config["mqtt"]["GEAR_D"]):
             gearshift = int(mqtt_config["mqtt"]["REMOTE_GEAR_D"])
-        elif int(gearshift) == int(mqtt_config["mqtt"]["GEAR_N"]):
+        elif int(data.gearshift) == int(mqtt_config["mqtt"]["GEAR_N"]):
             gearshift = int(mqtt_config["mqtt"]["REMOTE_GEAR_N"])
-        elif int(gearshift) == int(mqtt_config["mqtt"]["GEAR_R"]):
+        elif int(data.gearshift) == int(mqtt_config["mqtt"]["GEAR_R"]):
             gearshift = int(mqtt_config["mqtt"]["REMOTE_GEAR_R"])
-        elif int(gearshift) == int(mqtt_config["mqtt"]["GEAR_P"]):
+        elif int(data.gearshift) == int(mqtt_config["mqtt"]["GEAR_P"]):
             gearshift = int(mqtt_config["mqtt"]["REMOTE_GEAR_P"])
 
         pub_data = [
@@ -50,7 +59,6 @@ def vehicle_status_callback(data):
         ]
         pub_msg = convert_csv(pub_data)
         publish_mqtt_msg("vehicle_status", pub_msg)
-        vehicle_state_count = 0
 
 def twist_cmd_callback(data):
     target_speed = (data.twist.linear.x * 60 * 60) / 1000 # km/h
@@ -101,7 +109,7 @@ def publish_remote_cmd(msg):
             remote_cmd.vehicle_cmd.lamp_cmd.l = 1
         elif int(split_mgs[4]) == 2:
             remote_cmd.vehicle_cmd.lamp_cmd.r = 1
-        elif int(split_mgs[4]) == 2:
+        elif int(split_mgs[4]) == 3:
             remote_cmd.vehicle_cmd.lamp_cmd.l = 1
             remote_cmd.vehicle_cmd.lamp_cmd.r = 1
 
@@ -115,8 +123,17 @@ def publish_remote_cmd(msg):
 
         pub_remote_cmd.publish(remote_cmd)
 
-def on_message(client, userdata, msg):
-    publish_remote_cmd(msg.payload)
+def on_message_for_mqtt(client, userdata, msg):
+    try:
+        publish_remote_cmd(msg.payload)
+    except:
+        rospy.loginfo(traceback.format_exc())
+
+def on_message_for_aws_iot(client, userdata, msg):
+    try:
+        publish_remote_cmd(msg.payload)
+    except:
+        rospy.loginfo(traceback.format_exc())
 
 if __name__ == '__main__':
 
@@ -133,12 +150,31 @@ if __name__ == '__main__':
     config_file_path = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../mqtt_config.yml'))
     f = open(config_file_path, "r+")
     mqtt_config = yaml.load(f)
-    rospy.loginfo("[MQTT BROKER] ADDRESS: " + mqtt_config['mqtt']['ADDRESS'] + ", PORT: " + str(mqtt_config['mqtt']['PORT']))
+    rospy.loginfo("[MQTT BROKER] TYPE: " + mqtt_config['mqtt']['TYPE'] + ", ADDRESS: " + mqtt_config['mqtt']['ADDRESS'] + ", PORT: " + str(mqtt_config['mqtt']['PORT']))
 
-    mqtt_cient = mqtt.Client(client_id="vehicle_" + str(mqtt_config["mqtt"]["VEHICLEID"]), protocol=mqtt.MQTTv311)
-    mqtt_cient.on_connect = on_connect
-    mqtt_cient.on_message = on_message
-    mqtt_cient.connect(mqtt_config['mqtt']['ADDRESS'], port=mqtt_config['mqtt']['PORT'], keepalive=mqtt_config['mqtt']['TIMEOUT'])
-    mqtt_cient.loop_start()
+    if mqtt_config['mqtt']['TYPE'] == "MQTT":
+        mqtt_cient = mqtt.Client(client_id="vehicle_" + str(mqtt_config["mqtt"]["VEHICLEID"]), protocol=mqtt.MQTTv311)
+        mqtt_cient.on_connect = on_connect
+        mqtt_cient.on_message = on_message_for_mqtt
+        mqtt_cient.connect(mqtt_config['mqtt']['ADDRESS'], port=mqtt_config['mqtt']['PORT'], keepalive=mqtt_config['mqtt']['TIMEOUT'])
+        mqtt_cient.loop_start()
+
+    elif mqtt_config['mqtt']['TYPE'] == "AWS_IOT":
+        mqtt_cient = AWSIoTMQTTClient("vehicle_" + str(mqtt_config["mqtt"]["VEHICLEID"]))
+        mqtt_cient.configureEndpoint(mqtt_config['mqtt']['ADDRESS'], int(mqtt_config['mqtt']['PORT']))
+        cert_path = os.path.dirname(os.path.abspath(__file__)) + "/../cert/"
+        mqtt_cient.configureCredentials(
+            cert_path + mqtt_config['mqtt']['ROOTCA'],
+            cert_path + mqtt_config['mqtt']['PRICATE_KEY'],
+            cert_path + mqtt_config['mqtt']['CERTIFICATE']
+        )
+        mqtt_cient.configureAutoReconnectBackoffTime(1, 32, 20)
+        mqtt_cient.configureOfflinePublishQueueing(100)
+        mqtt_cient.configureDrainingFrequency(100)  # Draining: X Hz
+        mqtt_cient.configureConnectDisconnectTimeout(3600)
+        mqtt_cient.configureMQTTOperationTimeout(5)
+        mqtt_cient.connect()
+
+        mqtt_cient.subscribe("vehicle/" + str(mqtt_config['mqtt']['VEHICLEID']) + "/remote_cmd", 1, on_message_for_aws_iot)
 
     rospy.spin()
