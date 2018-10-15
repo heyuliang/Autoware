936d1cb43b07fc6ba6613b1d080cba01b49ff053
Masaya Kataoka <ms.kataoka@gmail.com>
2018-10-15 16:23:59 +0900

    add execute function

--- ros/src/system/core/autoware_core/include/autoware_core/node_launcher.h
+++ ros/src/system/core/autoware_core/include/autoware_core/node_launcher.h
@@ -1,11 +1,36 @@
 #ifndef NODE_LAUNCHER_H_INCLUDED
 #define NODE_LAUNCHER_H_INCLUDED
 
+//headers in STL
+#include <string>
+#include <vector>
+#include <mutex>
+
+//headers in boost
+#include <boost/thread.hpp>
+#include <boost/bind.hpp>
+
+struct launch_info
+{
+    const std::string package;
+    const std::string launch_filename;
+    launch_info(std::string package_, std::string launch_filename_) : package(package_), launch_filename(launch_filename_)
+    {
+
+    }
+};
+
 class node_launcher
 {
 public:
     node_launcher();
     ~node_launcher();
+    void launch(std::string package,std::string launch_filename);
+    std::vector<launch_info> get_launched_info();
+private:
+    std::mutex mtx_;
+    void execute_(launch_info info);
+    std::vector<launch_info> launched_info_;
 };
 
 #endif  //NODE_LAUNCHER_H_INCLUDED
--- ros/src/system/core/autoware_core/src/node_launcher.cpp
+++ ros/src/system/core/autoware_core/src/node_launcher.cpp
@@ -7,5 +7,49 @@ node_launcher::node_launcher()
 
 node_launcher::~node_launcher()
 {
-    
+
+}
+
+void node_launcher::launch(std::string package,std::string launch_filename)
+{
+    launch_info info(package,launch_filename);
+    return;
+}
+
+void node_launcher::execute_(launch_info info)
+{
+    mtx_.lock();
+    launched_info_.push_back(info);
+    mtx_.unlock();
+
+    mtx_.lock();
+    /*
+    for(auto itr=launched_info_.begin() ; itr!=launched_info_.end(); itr++)
+    {
+        if(itr->package == info.package && itr->launch_filename == info.launch_filename)
+        {
+            //launched_info_.erase(itr);
+            break;
+        }
+        /*
+        if(launched_info_[i].package == info.package && launched_info_[i].launch_filename == info.launch_filename)
+        {
+            launched_info_.erase(launched_info_.begin());
+            //launched_info_.erase(launched_info_.begin());
+            //launched_info_.begin() + (unsigned int)i;
+            //launched_info_.erase(launched_info_.begin() + (unsigned int)(i));
+            break;
+        }
+    }
+    */
+    mtx_.unlock();
+    return;
+}
+
+std::vector<launch_info> node_launcher::get_launched_info()
+{
+    mtx_.lock();
+    std::vector<launch_info> ret = launched_info_;
+    mtx_.unlock();
+    return ret;
 }
