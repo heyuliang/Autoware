c775140ec240b1c5fdfb0e42fd12e8ab714413e7
Yusuke FUJII <yusuke.fujii@tier4.jp>
2018-10-24 17:14:56 +0900

    remove unneccesary line

--- ros/src/computing/planning/decision/packages/decision_maker/include/decision_maker_events.hpp
+++ ros/src/computing/planning/decision/packages/decision_maker/include/decision_maker_events.hpp
@@ -1,13 +1,6 @@
 #ifndef __DECISION_MAKER_EVENT_HPP__
 #define __DECISION_MAKER_EVENT_HPP__
 
-#include <vector>
-
-#include <geometry_msgs/Point.h>
-#include <jsk_recognition_msgs/BoundingBox.h>
-
-#include <autoware_msgs/lane.h>
-
 namespace decision_maker
 {
 }
