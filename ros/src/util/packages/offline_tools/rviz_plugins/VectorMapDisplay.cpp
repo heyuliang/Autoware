50871a300921e87929b159ae2ec9be5dcb13ba0e
Adi Sujiwo <sujiwo@gmail.com>
2018-11-09 09:47:21 +0900

    X

--- ros/src/util/packages/offline_tools/rviz_plugins/VectorMapDisplay.cpp
+++ ros/src/util/packages/offline_tools/rviz_plugins/VectorMapDisplay.cpp
@@ -5,7 +5,50 @@
  *      Author: sujiwo
  */
 
+#include <vector>
 #include "VectorMapDisplay.h"
+
+
+using namespace std;
+
+
+const vector<string> VectorMapFileNames
+    {
+      "idx.csv",
+      "point.csv",
+      "vector.csv",
+      "line.csv",
+      "area.csv",
+      "pole.csv",
+      "box.csv",
+      "dtlane.csv",
+      "node.csv",
+      "lane.csv",
+      "wayarea.csv",
+      "roadedge.csv",
+      "gutter.csv",
+      "curb.csv",
+      "whiteline.csv",
+      "stopline.csv",
+      "zebrazone.csv",
+      "crosswalk.csv",
+      "road_surface_mark.csv",
+      "poledata.csv",
+      "roadsign.csv",
+      "signaldata.csv",
+      "streetlight.csv",
+      "utilitypole.csv",
+      "guardrail.csv",
+      "sidewalk.csv",
+      "driveon_portion.csv",
+      "intersection.csv",
+      "sidestrip.csv",
+      "curvemirror.csv",
+      "wall.csv",
+      "fence.csv",
+      "railroad_crossing.csv"
+    };
+
 
 
 void insertMarkerArray(visualization_msgs::MarkerArray& a1, const visualization_msgs::MarkerArray& a2)
@@ -14,9 +57,21 @@ void insertMarkerArray(visualization_msgs::MarkerArray& a1, const visualization_
 }
 
 
+template <class T, class U>
+U createObjectArray(const std::string& file_path)
+{
+	U obj_array;
+	// NOTE: Autoware want to use map messages with or without /use_sim_time.
+	// Therefore we don't set obj_array.header.stamp.
+	// obj_array.header.stamp = ros::Time::now();
+	obj_array.header.frame_id = "map";
+	obj_array.data = vector_map::parse<T>(file_path);
+	return obj_array;
+}
+
+
 VectorMapLoader::VectorMapLoader(const std::string &directory) :
-	vmDir(directory),
-	vector_map::VectorMap()
+	vmDir(directory)
 {
 	loadAll ();
 }
