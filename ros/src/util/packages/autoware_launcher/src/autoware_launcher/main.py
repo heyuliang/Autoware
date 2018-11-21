e10375b61f8148cfba6fe76eb3b9430b49a215fa
Isamu Takagi <isamu.takagi@tier4.jp>
2018-11-21 11:33:28 +0900

    Modify config for launch file tree structure

--- ros/src/util/packages/autoware_launcher/src/autoware_launcher/main.py
+++ ros/src/util/packages/autoware_launcher/src/autoware_launcher/main.py
@@ -1,16 +1,47 @@
+import os
 import subprocess
 
+
+from python_qt_binding.QtCore import Signal as QtSignal
+from python_qt_binding.QtCore import Slot   as QtSlot
 from python_qt_binding.QtWidgets import QApplication as AwApplication
+from python_qt_binding.QtWidgets import QSizePolicy
 from python_qt_binding.QtWidgets import QWidget
 from python_qt_binding.QtWidgets import QLabel
 from python_qt_binding.QtWidgets import QPushButton
+from python_qt_binding.QtWidgets import QFileDialog
 from python_qt_binding.QtWidgets import QHBoxLayout
 from python_qt_binding.QtWidgets import QVBoxLayout
+
+from profile import AwConfigNode
+
+class AwMainFrame(QWidget):
+
+    profile_selected = QtSignal(str)
+
+    def __init__(self):
+
+    	super(AwMainFrame, self).__init__()
+        hlayout = QHBoxLayout()
+        self.setLayout(hlayout)
+
+        profile_label = QLabel("Profile")
+        profile_label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
+        hlayout.addWidget(profile_label)
+
+        select_button = QPushButton("Select")
+        select_button.clicked.connect(self.select_profile)
+        hlayout.addWidget(select_button)
+
+    def select_profile(self):
+        path = QFileDialog.getExistingDirectory(self, "Select Config", os.path.expanduser("~/.autoware/profiles"))
+        self.profile_selected.emit(path + "/")'
 
 class AwMainWidget(QWidget):
 
     def __init__(self):
     	super(AwMainWidget, self).__init__()
+        self.profile = None
 
         button_root = QPushButton("Launch")
         button_root.setCheckable(True)
@@ -25,18 +56,21 @@ class AwMainWidget(QWidget):
         hlayout.addWidget(button_root)
         hlayout.addWidget(button_rviz)
 
+        frame_main = AwMainFrame()
+        frame_main.profile_selected.connect(self.on_profile_selected)
+
         vlayout = QVBoxLayout()
+        vlayout.addWidget(frame_main)
         vlayout.addWidget(AwMapFrame())
         vlayout.addLayout(hlayout)
         self.setLayout(vlayout)
 
-
     def launch_root(self, checked):
         if checked:
-            profiles = "/home/isamu-takagi/AutowareWork/autoware-launcher-py/ros/src/util/packages/autoware_launcher/profiles/"
-            self.proc_root = subprocess.Popen(["roslaunch", "root.launch"], cwd = profiles + "default")
+            if self.profile is not None:
+                self.proc_root = subprocess.Popen(["roslaunch", "root.launch"], cwd = profile.rootpath)
         else:
-            self.proc_root.terminate()
+                self.proc_root.terminate()
 
     def launch_rviz(self, checked):
         if checked:
@@ -44,6 +78,11 @@ class AwMainWidget(QWidget):
         else:
             self.proc_rviz.terminate()
 
+    @QtSlot(str)
+    def on_profile_selected(self, path):
+        self.profile = AwConfigNode.load(path)
+        self.profile.dump()
+        
 
 class AwMapFrame(QLabel):
 
@@ -52,3 +91,7 @@ class AwMapFrame(QLabel):
     	super(AwMapFrame, self).__init__()
         self.setText("Map Config")
         self.setStyleSheet("border: 1px solid;")
+
+if __name__ == "__main__":
+    path = "/home/isamu-takagi/AutowareWork/autoware-launcher-py/ros/src/util/packages/autoware_launcher/profiles/default/"
+    profile.load(path).dump()
--- ros/src/util/packages/autoware_launcher/src/autoware_launcher/profile.py
+++ ros/src/util/packages/autoware_launcher/src/autoware_launcher/profile.py
@@ -1,29 +1,69 @@
 import sys
-import xml.etree.ElementTree as ET
 
-class AwProfileNode(object):
+import os
+import xml.etree.ElementTree as ET
+import yaml
+
+
+
+class AwConfigNode(object):
 
     def __init__(self):
-        self.title = 'Untitled Profile'
+        self.nodepath = ""
         self.children = []
+        self.info = False
+        self.title = "[No information]"
 
     def dump(self, indent = 0):
-        print((' ' * indent) + self.title)
+        print((" " * indent) + "--------")
+        print((" " * indent) + "Title : " + self.title)
+        print((" " * indent) + "Path  : " + self.nodepath)
         for child in self.children:
             child.dump(indent + 2)
 
-def load(path):
-    return _load_node(path, 'root.launch')
+class AwConfigTree(object):
 
-def _load_node(path, file):
-    profile = AwProfileNode()
-    profile.title = file
-    root = ET.parse(path + file).getroot()
-    for child in root:
-        profile.children.append( _load_node(path, child.attrib.get('file')) )
-    return profile
+    def __init__(self, treepath):
+        self.rootnode = None
+        self.treepath = treepath
 
-if __name__ == '__main__':
-    profile = load(sys.argv[1])
-    profile.dump()
-    
+    def dump(self):
+        if self.rootnode is not None:
+            self.rootnode.dump()
+
+    def load(self):
+        self.rootnode = self.load_node("root")
+
+    def load_node(self, nodepath):
+
+        node = AwConfigNode()
+        node.nodepath = nodepath
+
+        # Read information file
+        try:
+            fullpath = self.treepath + nodepath + ".yaml"
+            if os.path.isfile(fullpath):
+                with open(fullpath) as file:
+                    info = yaml.safe_load(file)
+                    node.info = True
+                    node.title = info["title"]
+        except:
+            print("yaml open error")
+            pass
+
+        # Read launch file
+        if node.info:
+            fullpath = self.treepath + nodepath + ".launch"
+            root_xml = ET.parse(fullpath).getroot()
+            for child_xml in root_xml:
+                if child_xml.tag == "include":
+                    child_name, child_ext = os.path.splitext(os.path.basename(child_xml.attrib.get("file")))
+                    node.children.append( self.load_node(nodepath + os.path.sep + child_name) )
+
+        return node
+
+
+if __name__ == "__main__":
+    tree = AwConfigTree(sys.argv[1])
+    tree.load()
+    tree.dump()
